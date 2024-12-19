7b69b4c08534ecfb2397113f2c599625cf22681f
Rishitha1512 <katragadda.rishitha@kalvium.community>
2024-12-19 17:15:23 +0530

    completed milestone 9

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import Signup from './Pages/Signup';
 import Login from './Pages/Login';
+import ProductEntryPage from './Pages/ProductEntryPage';
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/product-entry-page" element={<ProductEntryPage />} />
       </Routes>
     </>
   );
