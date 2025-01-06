4418c20971e1f2fde6b4a330d8b1dd2a0d3f61b1
Rishitha1512 <katragadda.rishitha@kalvium.community>
2025-01-06 17:21:36 +0530

    completed milestone 12

--- FrontEnd/src/Pages/HomePage.jsx
+++ FrontEnd/src/Pages/HomePage.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react';
-import Card from '../component/ProductCard/Card';
+import { useEffect, useState } from "react";
+import Card from "../component/ProductCard/Card.jsx";
+import axios from "axios";
 
 function HomePage() {
-  const [data, setdata] = useState(
-    new Array(20).fill({ title: 'Product Title' })
-  );
+  const [data, setdata] = useState();
+  
+  const fetchProduct = async () => {
+    const res = await axios.get(
+      "http://localhost:8080/product/get-products"
+    );
+    setdata(res.data.data);
+  };
 
+  useEffect(() => {
+    const callhandle = async () => {
+      await fetchProduct();
+    };
+    callhandle();
+  }, []);
   console.log(data);
 
   return (
@@ -13,10 +25,22 @@ function HomePage() {
       <h1 className="text-center">Home Page for Follow Along</h1>
 
       <div className="grid grid-cols-3 gap-4">
-        {data.map((ele, index) => {
+        {data?.map((ele, index) => {
           return (
-            <div key={index} style={{ margin: 'auto' }}>
-              <Card title={ele.title} Index={index} />
+            <div
+              key={index}
+              style={{ margin: "auto" }}
+              className="border-white"
+            >
+              <Card
+                title={ele.title}
+                image={ele.images[0] ? ele.images[0] : "Product Image missing"}
+                Index={index}
+                description={ele.description}
+                originalPrice={ele.originalPrice}
+                discountedPrice={ele.discountedPrice}
+                rating={ele.rating}
+              />
             </div>
           );
         })}
