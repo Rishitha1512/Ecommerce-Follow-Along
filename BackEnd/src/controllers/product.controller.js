31048990ce32e79e61a0ac446f08ef996aa6ae21
Rishitha1512 <katragadda.rishitha@kalvium.community>
2025-01-09 14:31:24 +0530

    completed milestone 13

--- BackEnd/src/controllers/product.controller.js
+++ BackEnd/src/controllers/product.controller.js
@@ -73,4 +73,79 @@ const getProductDataController = async(req,res) => {
   }
 };
 // controller
-module.exports = { createProductController, getProductDataController };
+
+const updateProductController = async(req,res) => {
+  const {
+    title,
+    description,
+    rating,
+    discountedPrice,
+    originalPrice,
+    quantity,
+    category,
+  } = req.body;
+  const { id } = req.params;
+  try{
+    const checkIfProductExists = await ProductModel.findOne({ _id: id });
+  
+    if (!checkIfProductExists) {
+      return res.status(404).send({ message: 'Product Not Found' });
+    }
+    const arrayImage = req.files && req.files?.map(async (singleFile, index) => {
+      return cloudinary.uploader
+      .upload(singleFile.path,{
+        folder: 'uploads'
+      })
+      .then((result)=>{
+        fs.unlinkSync(singleFile.path);
+        return result.url;
+      });
+    });
+    const Imagedata = await Promise.all(arrayImage);
+    const findAndUpdate = await ProductModel.findByIdAndUpdate(
+      { _id: id},
+      {
+        title,
+        description,
+        rating,
+        discountedPrice,
+        originalPrice,
+        quantity,
+        category,
+        images: Imagedata,
+      },
+      {
+        new: true,
+      }
+    );
+
+    return res.status(201).send({
+      message: 'Document Updated Successfully',
+      success: true,
+      UpdatedResult: findAndUpdate,
+    });
+
+  }catch(er){
+    return res.status(500).send({ message: er.message, success: false });
+
+  }
+};
+
+const getSinglePRoductDocumentController = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await ProductModel.findOne({ _id: id });
+    console.log(data);
+    if (!data) {
+      return res.status(404).send({ Message: 'Product Not Found' });
+    }
+
+    return res
+      .status(200)
+      .send({ message: 'Product Successfully fetched', data, success: true });
+  } catch (er) {
+    return res.status(500).send({ message: er.message, success: false });
+  }
+};
+
+module.exports = { createProductController, getProductDataController,updateProductController,getSinglePRoductDocumentController };
--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './Pages/HomePage';
 import Signup from './Pages/Signup';
 import Login from './Pages/Login';
 import ProductEntryPage from './Pages/ProductEntryPage';
+import UpdateForm from './Pages/UpdateForm';
 function App() {
   return (
     <>
@@ -13,6 +14,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/product-entry-page" element={<ProductEntryPage />} />
+        <Route path="/update-form/:id" element={<UpdateForm />} />
       </Routes>
     </>
   );
