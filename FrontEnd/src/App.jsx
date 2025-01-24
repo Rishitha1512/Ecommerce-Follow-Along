f7343758db109d982c086895dcf7bfa5bff1020e
Rishitha1512 <katragadda.rishitha@kalvium.community>
2025-01-24 14:27:42 +0530

    completed milestone 21

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,17 +4,19 @@ import Signup from './Pages/Signup';
 import Login from './Pages/Login';
 import ProductEntryPage from './Pages/ProductEntryPage';
 import UpdateForm from './Pages/UpdateForm';
-import Navbar from './component/Navbar/navbar';
+// import Navbar from './component/Navbar/navbar';
 import SinglePageProduct from './Pages/SingleProductPage';
 import CartPage from './Pages/CartPage';
 import ProfilePage from './Pages/Profile';
+import AddressCard from './component/AddressComp/AddressCard';
 
 function App() {
   return (
     <>
       {/* <LoginPage /> */}
       {/* <SignupForm /> */}
-      <Navbar/>
+      <AddressCard/>
+      {/* <Navbar/> */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<Signup />} />
